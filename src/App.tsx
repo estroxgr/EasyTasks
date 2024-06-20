875376e4ce219cff17a9d4332bf1d7989446ba5a
estroxgr <alexanastagr@gmail.com>
2024-06-20 19:32:58 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+// components
+import TaskCard from "./components/TaskCard"
+
+function App() {
+
+  return (
+    <>
+    <div className="flex row gap-2 wrap">
+      <TaskCard/>
+      <TaskCard/>
+      <TaskCard/>
+      <TaskCard/>
+      <TaskCard/>
+      <TaskCard/>
+      <TaskCard/>
+      <TaskCard/>
+      <TaskCard/>
+      <TaskCard/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,19 @@
+export default function TaskCard() {
+    const styles = {
+        card: {
+            width: 200,
+            height: 200,
+            backgroundColor: '#fff',
+            borderRadius: 10
+        }
+    }
+    return (
+        <>
+            <div style={styles.card}>
+
+
+            </div>
+        </>
+    );
+}
+
